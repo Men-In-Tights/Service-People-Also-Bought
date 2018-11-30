3df3658251a5678e66478b81dd43e400b67313d1
woobin huh <madgb00@gmail.com>
2018-11-30 15:04:38 -0800

    wrong path

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
 
   componentDidMount() {
     let url = window.location.pathname.split('/');
-    let param = url[url.length - 1];
+    let param = url[url.length - 2];
     axios.get(`/api/alsoBought/${param}`)
       .then(response => {
         // let data = response.data;
